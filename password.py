4723296a757d44b479cdb2b15c42f68ee2e48dae
Rimsha30laraib <141603431+Rimsha30laraib@users.noreply.github.com>
2024-02-18 22:07:30 +0500

    Delete password.py

--- password.py
+++
@@ -1,7 +0,0 @@
-#CHECK THE PASSWORD IS RIGHT OR NOT
-password = input("What is the password? ")
-
-if password == "abc$123" or password == "ABC$123":
-    print("Welcome!")
-else:
-    print("I don't know you")
